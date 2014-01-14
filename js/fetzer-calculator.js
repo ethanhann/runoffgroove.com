3feed8c368f544e36424a5eeddebd424e29f4615
Ethan Hann <eh2413@columbia.edu>
2014-01-14 16:43:12 -0500

    Improve Fetzer calculator

--- js/fetzer-calculator.js
+++ js/fetzer-calculator.js
@@ -1,7 +1,6 @@
 <!-- Fetzer Design Formulas  -->
-jQuery(document).ready(function () {
-    jQuery('#fetzer-calculate-button').click(function (e) {
-        e.preventDefault();
+jQuery(document).ready(function() {
+    var calculateFetzerValues = function() {
         var vp = Math.abs(jQuery('#input-vp').val()),
             idss = Math.abs(jQuery('#input-idss').val()) / 1000,
             vcc = Math.abs(jQuery('#input-vcc').val());
@@ -15,5 +14,25 @@ jQuery(document).ready(function () {
         jQuery('#output-av-db').text(20 * Math.log(0.542 * (vcc / vp - 2)) / Math.log(10));
         jQuery('#output-vo').text(vcc - 2 * vp);
         jQuery('#output-cp').text(1000000000 / (0.83 * vp / idss * 6.283 * 1000));
+    };
+
+    var resetFetzerInputs = function() {
+        jQuery('#input-vp').val(0.83);
+        jQuery('#input-idss').val(0.64);
+        jQuery('#input-vcc').val(9);
+    };
+
+    jQuery('#fetzer-calculate-button').click(function(e) {
+        e.preventDefault();
+        calculateFetzerValues();
     });
+
+    jQuery('#fetzer-reset-button').click(function(e) {
+        e.preventDefault();
+        resetFetzerInputs();
+        calculateFetzerValues();
+    });
+
+    resetFetzerInputs();
+    calculateFetzerValues();
 });
